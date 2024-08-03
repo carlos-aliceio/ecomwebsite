69641086ed4221d453d8f19242d665f9b832684e
carlos-aliceio <findingpartners115@gmail.com>
2024-08-02 22:30:09 -0700

    Added Login page

--- website/website/urls.py
+++ website/website/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from shop import views
+from django.contrib.auth.views import LoginView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('<int:id>/',views.detail,name='detail'),
     path('checkout/',views.checkout,name='checkout'),
     path("register/",views.Register,name="register"),
+    path("login/",LoginView.as_view(template_name="base/login.html"),name="login"),
    
 ]
